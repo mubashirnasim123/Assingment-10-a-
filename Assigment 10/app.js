04005b619cfa09b0466cfb6eac0977fc24c90d0f
Mubashir Bin Nasim <100836260+mubashirnasim123@users.noreply.github.com>
2022-10-01 12:41:16 +0500

    Add files via upload

---
+++ Assigment 10/app.js
@@ -0,0 +1,21 @@
+function circle(){
+    var element = document.getElementById("change")
+    console.log(element.className);
+    element.className = "circle";
+    console.log(element.className);
+
+}
+function Rectangle(){
+    var element = document.getElementById("change")
+    console.log(element.className);
+    element.className = "Rectangle";
+    console.log(element.className);
+
+}
+function Square(){
+    var element = document.getElementById("change")
+    console.log(element.className);
+    element.className = "Square";
+    console.log(element.className);
+
+}
